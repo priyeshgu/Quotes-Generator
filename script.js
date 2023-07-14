9008f92431e657b92f2df6a3b1f7a69fac121ed4
Priyesh Gupta <priyeshg.gupta5@gmail.com>
2023-07-14 21:50:41 +0530

    linked with backend

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const genButton = document.getElementById("gen-button");
 genButton.addEventListener('click', () =>{
-    console.log("button-click");
-})
+    console.log("test");
+    fetch('http://localhost:3000/quote')
+    .then(response => response.text())
+    .then(quote => {
+        document.getElementById('quote-container').innerText = quote;
+    })
+    .catch(erron =>{
+        console.error('Error:',error);
+    });
+});
 
